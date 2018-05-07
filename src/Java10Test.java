55be55029945d266daaabeeb7fdfde383d7e6467
elton.zhong <elton.zhong@ringcentral.com>
2018-05-07 19:27:17 +0800

    java10

---
+++ src/Java10Test.java
@@ -0,0 +1,12 @@
+public class Java10Test {
+
+    /**
+     * how to support java10 with idea?
+     * idea just won't reconize java10's JDK HOME
+     * @param args
+     */
+    public static void main(String[] args) {
+
+//        var a = String.valueOf(1);
+    }
+}
