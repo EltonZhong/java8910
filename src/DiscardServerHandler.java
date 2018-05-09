0937449effdfeba8c6bc399db89bb3b1b05249a8
elton.zhong <elton.zhong@ringcentral.com>
2018-05-09 23:27:35 +0800

    netty

---
+++ src/DiscardServerHandler.java
@@ -0,0 +1,38 @@
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+        ctx.flush();
+    }
+
+    public void printChannelRead(ChannelHandlerContext ctx, final Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            System.out.println(msg);
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
--- test/Java10Test.java
+++ test/Java10Test.java
@@ -13,5 +13,21 @@ public class Java10Test {
         System.out.printf("12");
         System.out.printf(a);
         System.out.println(a instanceof String);
+        w:for(int i=1;i<=4;i++){
+            n:for(int j=1;j<=4;j++){
+                System.out.println("i="+i+",j="+j);
+                break w;
+            }
+        }
+
+        int i = 0;
+        do {
+            if (i ++ > 10) {
+                System.out.println(1);
+                break;
+            }
+            System.out.println(i);
+
+        } while (true);
     }
 }
